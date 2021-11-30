dd3365c3d4d86576df473c4c482c4ee3293cdc03
Emil <emiliano.milewski@gmail.com>
2021-11-30 14:13:33 +0000

    made row of cord squares but cant place them on top of board

--- src/modules/displayBoard.js
+++ src/modules/displayBoard.js
@@ -3,16 +3,25 @@ function displayBoard(player) {
     const playerBoard = player.game.board;
     const content = document.querySelector(".content");
     const boardContainer = document.createElement("div");
+    const boardBorder = document.createElement("div");
     boardContainer.classList.add("board");
     let i = 0;
+    for (let j = 0; j < 10; j++) {
+        const borderSquare = document.createElement("div");
+        borderSquare.classList.add("border-square");
+        boardBorder.appendChild(borderSquare);
+        boardBorder.classList.add("board-border");
+    }
     playerBoard.forEach(() => {
         const square = document.createElement("div");
+        square.textContent = i;
         square.classList.add("square");
         square.setAttribute("data-index", i);
         boardContainer.appendChild(square);
         i++;
     });
-    boardsContainer.appendChild(boardContainer);
+    boardsContainer.appendChild(boardBorder);
+    boardBorder.appendChild(boardContainer);
     content.appendChild(boardsContainer);
     return boardContainer;
 }
